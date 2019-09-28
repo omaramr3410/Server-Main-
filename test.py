98a1d9d28a0b2d716c080e938d519e35dfd5b7b8
mahmohfayed <55924435+mahmohfayed@users.noreply.github.com>
2019-09-28 16:50:51 -0400

    hi

---
+++ test.py
@@ -0,0 +1,43 @@
+import sys
+
+def main():
+
+	#for (amount) in cursor:
+	#	debt.append({'amount':amount});
+
+	speed = 'fast'
+	expense = {'rent':800,'util':300,'transp':150,'foodShop':50,'entertain':30,'misc':20}
+	debt = [{'amount':1200,'percent':.05},{'amount':300,'percent':.03},{'amount':600,'percent':.04},{'amount':2000,'percent':.10}]
+	priority = []
+	#for index,value in enumerate(priority):
+
+	income = 1500
+	spent = 0
+
+	for key, value in expense.items():
+		spent = value+spent
+
+	remaining = income - spent
+	
+	maxRate = 0
+	sorted(debt,key=lambda debt: debt["percent"])
+	
+
+	for i in range(0,len(debt),1):
+		print(debt[i])
+	"""for (percent) in debt:
+		if debt[percent] > maxRate:
+			maxRate = debt[percent]
+			priority.append(percent)
+			debt.remove(percent)
+
+	numDebt = len(debt)"""
+	
+
+	print(maxRate)
+	print(*priority)
+
+
+
+
+main()
